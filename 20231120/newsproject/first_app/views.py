b26227ac56a564d82fb3e3c3a322dc2126387b1a
djangouser <m-nunomura@stroog.com>
2023-11-20 09:21:02 +0900

    3-1end

--- 20231120/newsproject/first_app/views.py
+++ 20231120/newsproject/first_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    message = "Hello, world!"
+    return HttpResponse(message)
+
+def page(request,page_id):
+    message = "ページ"+str(page_id)
+    return HttpResponse(message)
