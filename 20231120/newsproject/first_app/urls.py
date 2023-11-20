bd56ecabe0e7742700671e5a7db57e2eb0aa6867
djangouser <m-nunomura@stroog.com>
2023-11-20 09:41:04 +0900

    3-2end

--- 20231120/newsproject/first_app/urls.py
+++ 20231120/newsproject/first_app/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="index"),
-    path("page/<int:page_id>/",views.page,name="page"),
+    path("article/",views.article,name="article"),
+    
 ]
--- 20231120/newsproject/first_app/views.py
+++ 20231120/newsproject/first_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
-def index(request):
-    message = "Hello, world!"
-    return HttpResponse(message)
-
-def page(request,page_id):
-    message = "ページ"+str(page_id)
-    return HttpResponse(message)
+def article(request):
+    template = loader.get_template("first_app/article.html")
+    context = {
+        "title":"記事のタイトル",
+        "content":"記事の本文",
+    }
+    return HttpResponse(template.render(context,request))
